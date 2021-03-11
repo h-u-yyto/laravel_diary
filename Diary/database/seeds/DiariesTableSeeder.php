8f3266b0e5ebc61b5b6076aa3f7746ac86a3c408
Yuto <>
2021-03-11 18:03:56 +0900

    認証機能 / user table / リレーション

--- Diary/database/seeds/DiariesTableSeeder.php
+++ Diary/database/seeds/DiariesTableSeeder.php
@@ -13,6 +13,8 @@ class DiariesTableSeeder extends Seeder
      */
     public function run()
     {
+        $user = DB::table('users')->first();
+        
         $dairies = [
             [
                 'title' => 'セブでプログラミング',
@@ -32,6 +34,7 @@ class DiariesTableSeeder extends Seeder
             DB::table('diaries')->insert([
                 'title' => $diary['title'],
                 'body' => $diary['body'],
+                'user_id' => $user->id,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
             ]);
