2f94270ff12e1810c85b30b8978ede2f72025364
Yuto <>
2021-03-11 17:14:37 +0900

    layout.blade.php / テンプレート

--- Diary/resources/views/diaries/create.blade.php
+++ Diary/resources/views/diaries/create.blade.php
@@ -1,41 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/app.css">
-    <title>新規投稿画面</title>
-</head>
-<body>
-    <section class="container m-5">
-        <div class="row justify-content-center">
-            <div class="col-8">
-                @if($errors->any())
-                    <ul>
-                        @foreach($errors->all() as $message)
-                            <li class="alert alert-danger">{{ $message }}</li>
-                        @endforeach
-                    </ul>
-                @endif
+@extends('layout')
 
-                <form action="{{ route('diary.store') }}" method="POST">
-                    @csrf
-                    <div class="form-group">
-                        <label for="title">タイトル</label>
-                        <input type="text" class="form-control" name="title" id="title" value="{{ old('title') }}">
-                    </div>
-                    <div class="form-group">
-                        <label for="body">本文</label>
-                        <textarea name="body" id="" cols="30" rows="10" class="form-control">{{ old('body') }}</textarea>
-                    </div>
-                    <div class="text-right">
-                        <button type="submit" class="btn btn-primary">投稿</button>
-                    </div>
-                </form>
-            </div>
+@section('title')
+新規投稿
+@endsection
+
+@section('content')
+<section class="container m-5">
+    <div class="row justify-content-center">
+        <div class="col-8">
+            @if($errors->any())
+                <ul>
+                    @foreach($errors->all() as $message)
+                        <li class="alert alert-danger">{{ $message }}</li>
+                    @endforeach
+                </ul>
+            @endif
+
+            <form action="{{ route('diary.store') }}" method="POST">
+                @csrf
+                <div class="form-group">
+                    <label for="title">タイトル</label>
+                    <input type="text" class="form-control" name="title" id="title" value="{{ old('title') }}">
+                </div>
+                <div class="form-group">
+                    <label for="body">本文</label>
+                    <textarea name="body" id="" cols="30" rows="10" class="form-control">{{ old('body') }}</textarea>
+                </div>
+                <div class="text-right">
+                    <button type="submit" class="btn btn-primary">投稿</button>
+                </div>
+            </form>
         </div>
-    </section>
-    
-</body>
-</html>
+    </div>
+</section>
+@endsection
